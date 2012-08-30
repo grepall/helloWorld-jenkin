b28817cbadb20c8fc2dbc47d70d27664ac725953
grepall <nelamer@ya.ru>
2012-08-31 01:43:11 +0300

    initial project version

---
+++ src/main/java/My/Hello/Word/app/App.java
@@ -0,0 +1,20 @@
+package My.Hello.Word.app;
+
+/**
+ * Hello world! (1st step)
+ * With two strings (2nd step)
+ * Which was commented just for lulz (3rd step) 
+ */
+public class App 
+{
+    public static void main11( String[] args )
+    {
+
+	 System.out.println( "Hello World! It is a test JAVA app for Jenkins." ); 
+/**	System.out.println( "That is another string in this project." ); 
+
+ errorIsHere! */
+    }
+}
+
+/** Hello Error! */
