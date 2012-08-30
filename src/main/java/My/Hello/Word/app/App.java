cebc361eb6104f16945f306814230623b0cca51a
grepall <nelamer@ya.ru>
2012-08-31 02:26:51 +0300

    version 1.15

--- src/main/java/My/Hello/Word/app/App.java
+++ src/main/java/My/Hello/Word/app/App.java
@@ -4,7 +4,7 @@ package My.Hello.Word.app;
  * Hello world! (1st step)
  * With two strings (2nd step)
  * Which was commented just for lulz (3rd step) 
- * And now that is a version 1.1.0 !
+ * And now that is a version 1.1.5 !
  */
 public class App 
 {
@@ -12,10 +12,10 @@ public class App
     {
 
 	System.out.println( "Hello World! It is a test JAVA app for Jenkins." ); 
-	System.out.println( "That is another string in this project." ); 
-/**
+/**	System.out.println( "That is another string in this project." ); 
+
  */
     }
 }
 
-/** Hello Error! */
+/** */
